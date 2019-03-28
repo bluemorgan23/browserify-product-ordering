9a130815ab1525f97d1fa4ec57636e1a5538dfce
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-28 18:01:30 -0500

    Added images to the products in the JSON and posted them to the DOM. I fixed some styling issues. Created a review section in the database.

--- src/scripts/htmlFactory.js
+++ src/scripts/htmlFactory.js
@@ -1,7 +1,7 @@
 // This file contains the module that contains the methods that deal with creating HTML.
 
 const htmlFactory = {
-    createHtmlElement: (element, text, id) => {
+    createHtmlElement: (element, text, id, href, src) => {
         const newElement = document.createElement(element);
         if(text){
             newElement.textContent = text;
@@ -9,14 +9,22 @@ const htmlFactory = {
         if(id){
             newElement.id = id;
         }
+        if(href){
+            newElement.setAttribute("href", href);
+        }
+        if(src){
+            newElement.setAttribute("src", src)
+        }
         return newElement;
     },
     HTMLforProduct: (productObject) => {
         const productContainer = htmlFactory.createHtmlElement("section", undefined, `productContainer--${productObject.id}`);
-        productContainer.appendChild(htmlFactory.createHtmlElement("h3", productObject.name));
-        productContainer.appendChild(htmlFactory.createHtmlElement("p", productObject.description));
-        productContainer.appendChild(htmlFactory.createHtmlElement("p", productObject.price));
-        productContainer.appendChild(htmlFactory.createHtmlElement("p", productObject.quantity));
+        productContainer.appendChild(htmlFactory.createHtmlElement("h2", productObject.name));
+        productContainer.appendChild(htmlFactory.createHtmlElement("img", undefined, `image--${productObject.id}`, undefined, productObject.image));
+        productContainer.appendChild(htmlFactory.createHtmlElement("h4", productObject.description));
+        productContainer.appendChild(htmlFactory.createHtmlElement("p", `$${productObject.price}`));
+        productContainer.appendChild(htmlFactory.createHtmlElement("p", `Quantity Available: ${productObject.quantity}`));
+        productContainer.innerHTML += "<hr/>"
         return productContainer;
     }
 }
--- src/scripts/navBar.js
+++ src/scripts/navBar.js
@@ -7,10 +7,11 @@ const navBarModule = {
         const displayContainer = document.querySelector("#display-container");
         const navBar = htmlFactory.createHtmlElement("nav", undefined, "navBar");
         navBar.appendChild(htmlFactory.createHtmlElement("h1", "Betsy", "navBar-header"));
-        navBar.appendChild(htmlFactory.createHtmlElement("a", "Categories", "navBar-categories"));
-        navBar.appendChild(htmlFactory.createHtmlElement("a", "Orders", "navBar-orders"));
-        navBar.appendChild(htmlFactory.createHtmlElement("a", "Log Out", "navBar-logOut"));
+        navBar.appendChild(htmlFactory.createHtmlElement("a", "Categories", "navBar-categories", "http://localhost:8080"));
+        navBar.appendChild(htmlFactory.createHtmlElement("a", "Orders", "navBar-orders", "http://localhost:8080"));
+        navBar.appendChild(htmlFactory.createHtmlElement("a", "Log Out", "navBar-logOut", "http://localhost:8080"));
         displayContainer.appendChild(navBar);
+        displayContainer.innerHTML += "<hr/>"
         return displayContainer;
     }
 }
